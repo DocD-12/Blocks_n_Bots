3095f26ed34bbccd315ebf44b2455161c2fe58cb
Artem Davletshin <artemdavletshin12@gmail.com>
2023-12-04 22:29:56 +0300

    mix 1

--- game.py
+++ game.py
@@ -2,18 +2,27 @@ import pygame
 import random
 import time
 
+# Define the colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+grey = (127, 127, 127)
 
 class Game:
     def __init__(self, size):
         self.grid = [[-1 if (i == 0 or i == size - 1 or j == 0 or j == size -1) else 0
                       for j in range(size)] for i in range(size)]
 
+        self.screen_width = 500
+        self.screen_height = 500
+        self.win = None
+
     def start(self):
         pygame.init()
 
-        screen_width = 1000
-        screen_height = 1000
-        win = pygame.display.set_mode((screen_width, screen_height))
+        self.win = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption("Capture The Coin Game")
 
         running = True
@@ -29,7 +38,7 @@ class Game:
             # player4.move(keys)
 
             # Отображение игрового поля
-            win.fill(grey)
+            self.win.fill(grey)
             self.draw()
 
             # Отображение ИИ ботов
@@ -37,13 +46,13 @@ class Game:
             # Отображение текущих очков
             font = pygame.font.Font(None, 36)
             text = font.render(f'Player 1: 0  Player 2: 0', True, white)
-            win.blit(text, (10, 10))
+            self.win.blit(text, (10, 10))
 
             # Отображение времени игры
             passed_time = int(time.time() - start_time)
             remaining_time = max(0, game_duration - passed_time)
             time_text = font.render(f'Time: {remaining_time // 60}:{remaining_time % 60:02d}', True, white)
-            win.blit(time_text, (screen_width - 150, 10))
+            self.win.blit(time_text, (self.screen_width - 150, 10))
 
             # Обновление экрана
             pygame.display.update()
@@ -56,7 +65,7 @@ class Game:
         print(*self.grid, sep='\n')
 
     def draw(self):
-        for x in range(0, screen_width, 50):
-            pygame.draw.line(win, white, (x, 0), (x, screen_height))
-        for y in range(0, screen_height, 50):
-            pygame.draw.line(win, white, (0, y), (screen_width, y))
+        for x in range(0, self.screen_width, 50):
+            pygame.draw.line(self.win, white, (x, 0), (x, self.screen_height))
+        for y in range(0, self.screen_height, 50):
+            pygame.draw.line(self.win, white, (0, y), (self.screen_width, y))
